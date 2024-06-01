50b5158836205f0a8ee9419dcffb3b51db300a0b
ThiagoRiosB <123420372+ThiagoRiosB@users.noreply.github.com>
2024-06-01 08:17:15 -0300

    Finalizei o layout do filtro

--- exercicio-02/src/app/angular-material/angular-material.module.ts
+++ exercicio-02/src/app/angular-material/angular-material.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from "@angular/core";
 
-import {MatListModule} from '@angular/material/list';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from "@angular/material/core";
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 
 @NgModule({
+  providers: [
+    provideNativeDateAdapter()
+  ],
   imports: [
     MatListModule,
-    MatDividerModule
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule
   ],
   exports: [
     MatListModule,
-    MatDividerModule
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule
   ],
 })
 
--- exercicio-02/src/app/components/components.module.ts
+++ exercicio-02/src/app/components/components.module.ts
@@ -1,17 +1,24 @@
-import {NgModule} from "@angular/core";
+import { NgModule } from "@angular/core";
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { FilterComponent } from './filter/filter.component';
+
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent,
+
   ],
   imports: [
-    AngularMaterialModule
+    AngularMaterialModule,
+    FormsModule
   ],
   exports: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent
   ],
 })
 
