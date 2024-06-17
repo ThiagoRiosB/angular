e1627604cea71737338f786c07c92fd8614ccd2c
ThiagoRiosB <123420372+ThiagoRiosB@users.noreply.github.com>
2024-06-17 19:03:06 -0300

    Criei a rota de transações.

--- 2-desafio-plataforma-usuario/src/app/app.routes.ts
+++ 2-desafio-plataforma-usuario/src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { GeneralComponent } from './components/general/general.component';
 import { BasicComponent } from './components/general/components/basic/basic.component';
 import { ContactComponent } from './components/general/components/contact/contact.component';
 import { AddressComponent } from './components/general/components/address/address.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+import { CreditComponent } from './components/transactions/components/credit/credit.component';
+import { DebitComponent } from './components/transactions/components/debit/debit.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +18,11 @@ export const routes: Routes = [
     component: GeneralComponent,
     children: [
       {
+        path: '',
+        redirectTo: 'basic',
+        pathMatch: 'full',
+      },
+      {
         path: 'basic',
         component: BasicComponent,
       },
@@ -25,6 +33,25 @@ export const routes: Routes = [
       {
         path: 'address',
         component: AddressComponent,
+      }
+    ]
+  },
+  {
+    path: 'transactions',
+    component: TransactionsComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'credit',
+        pathMatch: 'full'
+      },
+      {
+        path: 'credit',
+        component: CreditComponent,
+      },
+      {
+        path: 'debit',
+        component: DebitComponent,
       }
     ]
   }
--- 2-desafio-plataforma-usuario/src/app/components/general/general.component.ts
+++ 2-desafio-plataforma-usuario/src/app/components/general/general.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-general',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './general.component.html',
   styleUrl: './general.component.scss'
 })
