ac2c5a78e661c0bf0db25134e64dd20cae330d53
ThiagoRiosB <123420372+ThiagoRiosB@users.noreply.github.com>
2024-06-20 20:16:28 -0300

    Exercício de rotas finalizado.

--- 2-desafio-plataforma-usuario/src/app/app.routes.ts
+++ 2-desafio-plataforma-usuario/src/app/app.routes.ts
@@ -1,12 +1,5 @@
 import { Routes } from '@angular/router';
 import { InitialComponent } from './components/initial/initial.component';
-import { GeneralComponent } from './components/general/general.component';
-import { BasicComponent } from './components/general/components/basic/basic.component';
-import { ContactComponent } from './components/general/components/contact/contact.component';
-import { AddressComponent } from './components/general/components/address/address.component';
-import { TransactionsComponent } from './components/transactions/transactions.component';
-import { CreditComponent } from './components/transactions/components/credit/credit.component';
-import { DebitComponent } from './components/transactions/components/debit/debit.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
@@ -18,51 +11,12 @@ export const routes: Routes = [
   {
     path: 'general',
     title: 'Geral',
-    component: GeneralComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'basic',
-        pathMatch: 'full',
-      },
-      {
-        path: 'basic',
-        title: 'Básico',
-        component: BasicComponent,
-      },
-      {
-        path: 'contact',
-        title: 'Contato',
-        component: ContactComponent,
-      },
-      {
-        path: 'address',
-        title: 'Endereço',
-        component: AddressComponent,
-      }
-    ]
+    loadChildren: () => import('./components/general/general.routes').then(m => m.GeneralRoutes),
   },
   {
     path: 'transactions',
     title: 'Transações',
-    component: TransactionsComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'credit',
-        pathMatch: 'full'
-      },
-      {
-        path: 'credit',
-        title: 'Crédito',
-        component: CreditComponent,
-      },
-      {
-        path: 'debit',
-        title: 'Débito',
-        component: DebitComponent,
-      }
-    ]
+    loadChildren: () => import('./components/transactions/transactions.routes').then(m => m.TransactionsRoutes),
   },
   {
     path: '**',
